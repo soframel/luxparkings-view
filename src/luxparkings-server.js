e91a886a346eb353f76d6502b7dddcf3309e700f
soframel <sophie@massen-ramel.net>
2020-01-04 19:45:23 +0100

    parameterized server url

--- src/luxparkings-server.js
+++ src/luxparkings-server.js
@@ -1,13 +1,16 @@
 import axios from 'axios';
 
+const serverUrl=process.env.VUE_APP_SERVER_URL;
+console.log("loaded server URL="+serverUrl);
+
 export async function getParkingNames () {
  //call luxparkings server to get parking names
   console.log("calling list of parkings");
-  return axios.get('http://localhost:3000/parkings');
+  return axios.get(serverUrl+"/parkings");
 }
 
 export async function getAverageFullTime(date, parkingName){  
   const dateS=date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate();
   console.log("fetching average full time for "+dateS+" and "+parkingName);
-  return axios.get("http://127.0.0.1:3000/averageFullTime?date="+dateS+"&parking="+parkingName);
+  return axios.get(serverUrl+"/averageFullTime?date="+dateS+"&parking="+parkingName);
 }
